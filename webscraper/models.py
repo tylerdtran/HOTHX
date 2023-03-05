24ae42849e018d14dd7f7677c5489d411ac20090
Alex West <awest2025@ucla.edu>
2023-03-05 11:21:16 -0800

    added Events model

--- webscraper/models.py
+++ webscraper/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    name = models.CharField(max_length=200)
+    link = models.CharField(max_length=2083, default="", unique=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    location = models.CharField(max_length=200, default="")
