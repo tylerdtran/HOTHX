c43405e9ed9f3385378743cc8147b4172eb8c622
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-03-05 10:31:25 -0800

    home page

--- hothx/urls.py
+++ hothx/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from . import views
+from .views import HomePageView
 
 urlpatterns = [
-    path("", views.index, name='index'),
+    path("", HomePageView.as_view(), name='home'),
     path('admin/', admin.site.urls),
 ]
